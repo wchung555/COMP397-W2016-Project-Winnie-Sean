ab2981993e01a314d8d41b33f7b59147bc66d35b
Sean Smith <srsmith92@gmail.com>
2016-04-21 16:19:35 -0400

    Configured Level 1-2 page

--- Scripts/scenes/level1-2.js
+++ Scripts/scenes/level1-2.js
@@ -3,7 +3,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     function __() { this.constructor = d; }
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
-// LEVEL 1.5 SCENE
+// INSTRUCTIONS SCENE
 var scenes;
 (function (scenes) {
     var Level12 = (function (_super) {
@@ -15,35 +15,41 @@ var scenes;
         // PUBLIC METHODS +++++++++++++++++++++
         // Start Method
         Level12.prototype.start = function () {
-            // add world to the scene
-            this._background1 = new objects.World("L2_Platform");
-            this.addChild(this._background1);
-            // add player to the scene
-            this._player = new objects.Player();
-            this.addChild(this._player);
-            // add label, start button to scene
-            this._sceneLabel = new objects.Label("Level 2", "60px Consolas", "#FFFFFF", config.Screen.CENTER_X, config.Screen.CENTER_Y, false);
-            this._sceneLabel.regX = this._sceneLabel.getBounds().width * 0.5;
-            this._sceneLabel.regY = this._sceneLabel.getBounds().height * 0.5;
-            this.addChild(this._sceneLabel);
-            this._startButton = new objects.Button("playButton", config.Screen.CENTER_X, config.Screen.CENTER_Y + 100, false);
-            this._startButton.regX = this._startButton.width * .5;
-            this._startButton.regY = this._startButton.height * .5;
+            //Game Music            
+            bgm = createjs.Sound.play("backgroundMusic", { loop: -1 });
+            //Add background
+            this._background = new createjs.Bitmap(assets.getResult("Pre2"));
+            this.addChild(this._background);
+            // add the Start button to the INSTRUCTIONS scene
+            this._startButton = new objects.Button("playButton", 512, 220, false);
             this.addChild(this._startButton);
+            // Start Button event listener
             this._startButton.on("click", this._startButtonClick, this);
+            // add the Exit button to the MENU scene
+            this._exitButton = new objects.Button("quitButton", 55, 220, false);
+            this.addChild(this._exitButton);
+            // Exit Button event listener
+            this._exitButton.on("click", this._exitButtonClick, this);
             // add this scene to the global stage container
             stage.addChild(this);
         };
-        // PLAY Scene updates here
+        // INTRO Scene updates here
         Level12.prototype.update = function () {
-            this._background1.update();
         };
         //EVENT HANDLERS ++++++++++++++++++++
         // START Button click event handler
         Level12.prototype._startButtonClick = function (event) {
             createjs.Sound.play("select");
-            // Switch to the LEVEL2 Scene
+            // Switch to the PLAY Scene
             scene = config.Scene.LEVEL2;
+            changeScene();
+        };
+        // EXIT Button click event handler
+        Level12.prototype._exitButtonClick = function (event) {
+            createjs.Sound.play("select");
+            // Switch to the END Scene
+            scene = config.Scene.END;
+            bgm.stop();
             changeScene();
         };
         return Level12;
